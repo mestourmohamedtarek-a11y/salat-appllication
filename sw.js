18f2b6fe58c641302deabbc91e3a37fd9ddaa864
mestourmohamedtarek-a11y <mestourmohamedtarek@gmail.com>
2026-09-19 12:57:58 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "salat-app-v2";
+
+const FILES_TO_CACHE = [
+    "./moh.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
